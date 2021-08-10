0b0a45aaf431cd0303b38fc92c4205403e8bda51
direyesm <37608963+direyesm@users.noreply.github.com>
2021-08-10 09:42:25 -0400

    Final Consumo Api

--- src/components/PredefinedFoodDishes.js
+++ src/components/PredefinedFoodDishes.js
@@ -5,11 +5,35 @@ import { predefinedFoodDishes } from "./../constants";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 
 const PredefinedFoodDishes = ({ getData, setHits }) => {
-  const getFoods = (title) => {};
+  const getFoods = (title) => {
+    predefinedFoodDishes.map((foodDish) => {
+      foodDish.selected = foodDish.title === title ? !foodDish.selected : false;
+      return !foodDish.selected
+        ? setHits()
+        : getData(
+            document.querySelector(`#${title}`).value.toLowerCase().trim()
+          );
+    });
+  };
 
   return (
-    <Grid>
-      <Paper></Paper>
+    <Grid item xs={12}>
+      <Paper className="paper">
+        {predefinedFoodDishes.map((foodDish) => {
+          const { title, selected } = foodDish;
+          return (
+            <ToggleButton
+              key={title}
+              id={title}
+              value={title}
+              selected={selected}
+              onClick={() => getFoods(title)}
+            >
+              {title}
+            </ToggleButton>
+          );
+        })}
+      </Paper>
     </Grid>
   );
 };
